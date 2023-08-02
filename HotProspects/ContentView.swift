fd0328f3d3c2df17cfb39772be76acc1bda534dd
keenan-chiasson <keenan.chiasson@acstexas.com>
2023-08-02 16:10:11 -0500

    manually publish observable object changes

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,24 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedTab = "One"
+    @StateObject var updater = DelayedUpdater()
     
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Text("Tab 1")
-                .onTapGesture {
-                    selectedTab = "Two"
-                }
-                .tabItem {
-                    Label("One", systemImage: "star")
-                }
-                .tag("One")
-            
-            Text("Tab 2")
-                .tabItem {
-                    Label("Two", systemImage: "circle")
-                }
-                .tag("Two")
+        Text("Value is: \(updater.value)")
+    }
+}
+
+@MainActor class DelayedUpdater: ObservableObject {
+    var value = 0 {
+        willSet {
+            objectWillChange.send()
+        }
+    }
+    
+    init() {
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
+            }
         }
     }
 }
