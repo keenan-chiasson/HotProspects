76de757db8a87defec123c503d9d3125518f6726
keenan-chiasson <keenan.chiasson@acstexas.com>
2023-08-02 12:03:06 -0500

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Keenan Chiasson on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
