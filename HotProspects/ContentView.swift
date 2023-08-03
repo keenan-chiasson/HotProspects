e8f36398a228e0b3dd2dc3a6c7e1ba4dd009e81b
keenan-chiasson <keenan.chiasson@acstexas.com>
2023-08-03 10:06:57 -0500

    create a context menu

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,29 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Image("example")
-            .interpolation(.none)
-            .resizable()
-            .scaledToFit()
-            .frame(maxHeight: .infinity)
-            .background(.black)
-            .ignoresSafeArea()
-    }
-}
-
-@MainActor class DelayedUpdater: ObservableObject {
-    var value = 0 {
-        willSet {
-            objectWillChange.send()
-        }
-    }
+    @State private var backgroundColor = Color.red
     
-    init() {
-        for i in 1...10 {
-            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
-                self.value += 1
-            }
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+                .padding()
+                .background(backgroundColor)
+            
+            Text("Change Color")
+                .padding()
+                .contextMenu {
+                    Button(role: .destructive) {
+                        backgroundColor = .red
+                    } label : {
+                        Label("Red", systemImage: "checkmark.circle.fill")
+                    }
+                    Button {
+                        backgroundColor = .green
+                    } label : {
+                        Label("Green", systemImage: "checkmark.circle.fill")
+                            .foregroundColor(.green)
+                    }
+                    Button {
+                        backgroundColor = .blue
+                    } label : {
+                        Label("Blue", systemImage: "checkmark.circle.fill")
+                            .foregroundColor(.blue)
+                    }
+                }
         }
     }
 }
