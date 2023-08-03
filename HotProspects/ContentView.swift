f5c2053ec8f8ddee9876993e2037c04eb36e78b4
keenan-chiasson <keenan.chiasson@acstexas.com>
2023-08-03 09:41:36 -0500

    test Swift's Result type

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,10 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject var updater = DelayedUpdater()
+    @State private var output = ""
     
     var body: some View {
-        Text("Value is: \(updater.value)")
+        Text(output)
+            .task {
+                await fetchReadings()
+            }
+    }
+    
+    func fetchReadings() async {
+        let fetchTask = Task { () -> String in
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) readings"
+        }
+        
+        // this is how we can pass the task object around
+        let result = await fetchTask.result
+        
+// Can do this
+//        do {
+//            output = try result.get()
+//        } catch {
+//            output = "Error: \(error.localizedDescription)"
+//        }
+// Or this
+        switch result {
+        case .success(let str):
+            output = str
+        case .failure(let error):
+            output = "Error: \(error.localizedDescription)"
+        }
     }
 }
 
