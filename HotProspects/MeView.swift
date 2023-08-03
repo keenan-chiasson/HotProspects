f519b777f328e8c5739f8c84e8e22a03409f1ea3
keenan-chiasson <keenan.chiasson@acstexas.com>
2023-08-03 13:16:44 -0500

    generate and scale up QR code

--- HotProspects/MeView.swift
+++ HotProspects/MeView.swift
@@ -5,11 +5,48 @@
 //  Created by Keenan Chiasson on 8/3/23.
 //
 
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct MeView: View {
+    @State private var name = "Keenan Chiasson"
+    @State private var emailAddress = "keenan.chiasson@acstexas.com"
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+                
+                TextField("Email address", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .font(.title3)
+                    .fontWeight(.bold)
+                
+                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+//                    .frame(width: 200, height: 200)
+            }
+            .navigationTitle("Your code")
+        }
+    }
+    
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+        
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+        
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
